9ff1cfaeb0566540c445e300cfafc1c06d5c284e
zoltantucs-gh <zoltantucs@gmail.com>
2026-08-15 09:55:50 +0200

    most feltöltöttem a 2 hiányzót!

--- server.js
+++ server.js
@@ -309,4 +309,9 @@ function getPublicGamesList() {
   }));
 }
 
-server.listen(3000, () => console.log('Szerver fut a http://localhost:3000 címen'));
+// server.listen(3000, () => console.log('Szerver fut a http://localhost:3000 címen'));
+
+const PORT = process.env.PORT || 3000;
+server.listen(PORT, () => {
+  console.log(`A szerver fut a ${PORT} porton`);
+});
